f1d06bfb0b1f66ed25accb17635ea5811fbc6f7a
joao3872 <joaomatheus11@gmail.com>
2022-06-23 23:08:03 -0300

    arquivos

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function abrirModal() {
+    var abrir = document.querySelector('section.modal_redefinir')
+
+    abrir.style.display = 'block'
+}
+
+
+function fecharModal() {
+    var fechar = document.querySelector('section.modal_redefinir')
+
+    fechar.style.display = 'none'
+}
