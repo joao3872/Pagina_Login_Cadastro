5d37f1488034f5048b356c74bada2391e3b77fd8
João Matheus <joaomatheus11@gmail.com>
2022-12-05 03:53:47 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+let inputSenha = document.querySelector('.senha')
+
+let repetirSenha = document.querySelector('.repetir_senha')
+
+// Elementos usados para exibir e ocultar a senha.
+let exibirSenha = document.querySelector('.exibirSenha')
+
+let exibirSenhaText = document.querySelector('.exibirText')
+
+let iconExibir = document.querySelector('#exibir')
+
+
+
 function abrirModal() {
     var abrir = document.querySelector('section.modal_redefinir')
 
@@ -11,3 +24,23 @@ function fecharModal() {
     fechar.style.display = 'none'
 }
 
+
+
+// Exibe e Oculta os caracteres do input de senha e do input de repetir senha.
+exibirSenha.addEventListener('click', () => {
+    if (inputSenha.type === "password" && repetirSenha.type === "password") {
+        inputSenha.type = 'text'
+        repetirSenha.type = 'text'
+
+        exibirSenhaText.textContent = 'Ocultar Senha'
+        iconExibir.style.color = 'var(--orange)'
+        exibirSenhaText.style.color = 'var(--orange)'
+    } else {
+        inputSenha.type = 'password'
+        repetirSenha.type = 'password'
+
+        exibirSenhaText.textContent = 'Exibir Senha'
+        iconExibir.style.color = 'var(--green)'
+        exibirSenhaText.style.color = 'var(--green)'
+    }
+})
