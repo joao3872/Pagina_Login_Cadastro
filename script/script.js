2e048b2d5ac3a0c3bba430e748812c0adbc6a346
João Matheus <joaomatheus11@gmail.com>
2022-12-05 12:04:48 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,6 +2,14 @@ let inputSenha = document.querySelector('.senha')
 
 let repetirSenha = document.querySelector('.repetir_senha')
 
+
+let avisoSenha = document.querySelector('.avisoSenha')
+
+
+// Botão de submit do formulário.
+let btnEnviar = document.querySelector('.btn_cadastrar')
+
+
 // Elementos usados para exibir e ocultar a senha.
 let exibirSenha = document.querySelector('.exibirSenha')
 
@@ -23,6 +31,22 @@ function fecharModal() {
 
     fechar.style.display = 'none'
 }
+
+
+// Desabilita o botão de submit, se os caracteres do input de senha, for diferente dos caracteres do input de repetir senha e exibe uma mensagem de aviso.
+repetirSenha.addEventListener('keyup', () => {
+    if (inputSenha.value != repetirSenha.value) {
+        btnEnviar.setAttribute('disabled', true)
+        btnEnviar.classList.add('btn_disabled')
+        
+        avisoSenha.style.display = 'block'
+    } else {
+        btnEnviar.removeAttribute('disabled')
+        btnEnviar.classList.remove('btn_disabled')
+        
+        avisoSenha.style.display = 'none'
+    }
+})
 
 
 
